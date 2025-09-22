751f7e7f2912540eff70d8b154340c04b9920ffa
sgt1796 <sgt1796@gmail.com>
2025-09-22 10:46:55 -0400

    update pypl setup to 0.3.2

--- PyPI-POP-guotai/POP/__init__.py
+++ PyPI-POP-guotai/POP/__init__.py
@@ -3,14 +3,13 @@ POP - Prompt Oriented Programming
 Reusable, mutable, prompt functions for LLMs.
 
 Author: Guotai Shen
-Version: 0.3.1
 """
 
 from .POP import PromptFunction
 from .Embedder import Embedder
 
 # Versioning
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __author__ = "Guotai Shen"
 __license__ = "MIT"
 
--- PyPI-POP-guotai/setup.py
+++ PyPI-POP-guotai/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="POP-guotai",  # Package name
-    version="0.3.1",  # Version number
+    version="0.3.2",  # Version number
     author="Guotai Shen",
     author_email="sgt1796@gmail.com",  
     description="Reusable, mutable, prompt functions for LLMs.",
